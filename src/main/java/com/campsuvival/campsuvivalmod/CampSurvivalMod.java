91529b69eaf715acff8929d134743fbcabca4464
pentantan <shutux500@gmail.com>
2017-03-13 08:11:31 +0900

    テクスチャモデルの変更

--- src/main/java/com/campsuvival/campsuvivalmod/CampSurvivalMod.java
+++ src/main/java/com/campsuvival/campsuvivalmod/CampSurvivalMod.java
@@ -19,21 +19,19 @@ public class CampSurvivalMod
     public static final String MOD_DEPENDENCIES = "required-after:Forge@[1.10.2-12.18.3.2254,)";
     public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.10.2]";
     /** Woodenframeのブロックのインスタンスを格納する */
-    public static Block woodenframeBlock;
+    public static Block woodenframeblock;
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
-        woodenframeBlock = new WoodenFrameBlock();
+        woodenframeblock = new WoodenFrameBlock();
         //ブロックの登録。登録文字列はMOD内で被らなければ何でも良い。
         ResourceLocation registryName = new ResourceLocation(MODID, "woodenframeblock");
-        ItemBlock woodenframeItemBlock = new ItemBlock(woodenframeBlock);
-        GameRegistry.register(woodenframeBlock, registryName);
-        GameRegistry.register(woodenframeItemBlock, registryName);
+        ItemBlock woodenframeitemblock = new ItemBlock(woodenframeblock);
+        GameRegistry.register(woodenframeblock, registryName);
+        GameRegistry.register(woodenframeitemblock, registryName);
 
-        //テクスチャ・モデル指定JSONファイル名の登録。
-        //今回は基本的な説明に留めるため、BlockStateについては解説しない。
-        //詳しくは1.8のブロック追加を見るように。
+        //テクスチャ・モデル指定JSONファイル名の登録
         if(event.getSide().isClient()) {
-            ModelLoader.setCustomModelResourceLocation(woodenframeItemBlock, 0, new ModelResourceLocation(new ResourceLocation(MODID, "woodenframeblock"), "inventory"));
+            ModelLoader.setCustomModelResourceLocation(woodenframeitemblock, 0, new ModelResourceLocation(new ResourceLocation(MODID, "woodenframeblock"), "inventory"));
         }
     }
 }
