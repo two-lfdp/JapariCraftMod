5fedc5a362c934212265eb3b29cf4931969a07b2
pentantan <shutux500@gmail.com>
2017-07-04 22:25:19 +0900

    プレイヤーを見る距離を変更

--- src/main/java/com/japaricraft/japaricraftmod/mob/Shoebill.java
+++ src/main/java/com/japaricraft/japaricraftmod/mob/Shoebill.java
@@ -41,7 +41,7 @@ public class Shoebill extends EntityTameable {
         this.tasks.addTask(6, new EntityAIMate(this, 1.0D));
         this.tasks.addTask(6, new EntityAIWander(this, 1.0D));
         this.tasks.addTask(7, new EntityAILookIdle(this));
-        this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 7.0F));
         this.targetTasks.addTask(1, new EntityAIOwnerHurtByTarget(this));
         this.targetTasks.addTask(2, new EntityAIOwnerHurtTarget(this));
         this.targetTasks.addTask(3, new EntityAIHurtByTarget(this, true));
