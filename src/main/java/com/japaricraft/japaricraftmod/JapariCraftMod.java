ecdf578473143705a73c108b0f1d57bdabc8ff53
pentantan <shutux500@gmail.com>
2017-10-04 22:03:05 +0900

    3.0.2　インベントリのアイテムが減る問題を直した

--- src/main/java/com/japaricraft/japaricraftmod/JapariCraftMod.java
+++ src/main/java/com/japaricraft/japaricraftmod/JapariCraftMod.java
@@ -40,7 +40,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class JapariCraftMod {
 
     public static final String MODID = "japaricraftmod";
-    public static final String VERSION = "3.0.1";
+    public static final String VERSION = "3.0.2";
     public static final String MODNAME = "JapariCraftMod";
 
 
